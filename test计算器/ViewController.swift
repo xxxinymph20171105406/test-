351b8c0bb4022c65121a7a916915b7f632c3c6e9
xxxinymph <839203420@qq.com>
2018-12-19 09:43:01 +0800

    没成功
     能运行但是 里面操作不对

--- test计算器/ViewController.swift
+++ test计算器/ViewController.swift
@@ -10,6 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lb1: UILabel!
+    var userIsInTheMiddleOfTypingANumber:Bool = false
+    
+    @IBAction func append7(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber{
+            lb1.text = lb1.text! + digit
+        }else{
+            lb1.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
+    }
+    
+    
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            enter()
+            
+        }
+        switch operation{
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        default: break
+    }
+}
+func performOperation(operation: (Double,Double)->Double ){
+        if operandStack.count >= 2{
+            lb1Value = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+            
+        }
+    }
+    private func perfiomOperation(operation: (Double) ->Double){
+        if operandStack.count >= 1{
+        lb1Value = operation(operandStack.removeLast())
+        enter()
+    }
+        
+}
+    var operandStack = Array<Double>()
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(lb1Value)
+        print(" operandStack = \(operandStack)")
+        
+    }
+    var lb1Value: Double{
+        get{
+            return NumberFormatter().number(from: lb1.text!)!.doubleValue
+        }
+        set{
+            lb1.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+         }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
